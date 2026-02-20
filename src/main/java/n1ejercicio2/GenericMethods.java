95b1481e998b7606bb1e5022ff82611e95b6bded
AlessandroPesole <alespesole@gmail.com>
2026-02-20 02:05:08 +0100

    S1.06 small changes

--- src/main/java/n1ejercicio2/GenericMethods.java
+++ src/main/java/n1ejercicio2/GenericMethods.java
@@ -1,7 +1,6 @@
 package n1ejercicio2;
 
 public class GenericMethods {
-
     public static <T, S, U> void printElements(T t, S s, U u) {
         System.out.println(t + "\n" + s + "\n" + u);
     }
--- src/main/java/n2ejercicio2/VarargsMethod.java
+++ src/main/java/n2ejercicio2/VarargsMethod.java
@@ -1,7 +1,6 @@
 package n2ejercicio2;
 
 public class VarargsMethod {
-
     public static <T> void printAll(T... elements) {
         for (T t : elements) {
             System.out.println(t);
